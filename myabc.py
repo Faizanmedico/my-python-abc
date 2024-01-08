8f5ce2628f6bd4967896948d5e0e7a1d35cd0adc
Sultan Ahmed <146020547+Faizanmedico@users.noreply.github.com>
2024-01-08 23:48:46 +0500

    Add files via upload

---
+++ myabc.py
@@ -0,0 +1,75 @@
+print("Hello, World!")
+if 5 > 2:
+  print("Five is greater than two!")
+  
+  x = 5
+y = "Hello, World!"
+
+print(x)
+print(y)
+
+
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+
+print(myvar)
+print(my_var)
+print(_my_var)
+print(myVar)
+print(MYVAR)
+print(myvar2)
+
+
+x, y, z = "Orange", "Banana", "Cherry"
+
+print(x)
+print(y)
+print(z)
+
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+x = "awesome"
+
+def myfunc():
+  print("Python is " + x)
+
+myfunc()
+
+x = "awesome"
+
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x)
+
+x = list(("apple", "banana", "cherry"))
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+x = set(("apple", "banana", "cherry"))
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+
+
+
+
